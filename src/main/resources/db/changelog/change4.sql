2a42cf79a740bc349ac6362ea18facd22436dd61
rajeshborade5 <rajesh.borade@tradingtechnologies.com>
2023-10-19 23:46:21 +0530

    initial commit

--- src/main/resources/db/changelog/change4.sql
+++ src/main/resources/db/changelog/change4.sql
@@ -1,11 +1,11 @@
 INSERT INTO tradable (id, name, amt)
 SELECT g.id, 'trade' || g.id, g.id
-FROM generate_series(1, 10000) AS g (id) ;
+FROM generate_series(1, 500000) AS g (id) ;
 
 INSERT INTO quote (id, quote)
 SELECT g.id, 'quote' || g.id
-FROM generate_series(1, 10000) AS g (id) ;
+FROM generate_series(1, 500000) AS g (id) ;
 
 INSERT INTO order_
 SELECT g.id, 'order_' || g.id
-FROM generate_series(1, 10000) AS g (id) ;
+FROM generate_series(1, 500000) AS g (id) ;
